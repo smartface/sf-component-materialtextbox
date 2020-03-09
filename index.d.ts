0c203eaa67d2cbec25001ec276c9b7c9befca6e1
Furkan Arabacı <furkan.arabaci@smartface.io>
2020-03-09 22:20:50 +0300

    fix(index.d.ts): Proper variables

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-import View = require('sf-core/ui/view');
-export = MaterialBox;
-// TODO: Detail of class MaterialTextBox
-declare type MaterialTextBox = any;
-declare class MaterialBox extends View {
+import FlexLayout = require('sf-core/ui/flexlayout');
+import MaterialTextBox = require('sf-core/ui/materialtextbox');
+export = FlMaterialTextBox;
+declare class FlMaterialTextBox extends FlexLayout {
     set onDropDownClick(onTouchEnded: () => void): void;
+    enableDropDown: boolean;
     showHideEnabled: boolean;
     clearAllEnabled: boolean;
-    options: any;
+    options: Object;
     materialTextBox: MaterialTextBox;
 }
